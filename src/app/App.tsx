4239846e898478fe0370b061ae3225afee5b44cd
LorenzoTaylor <lmtaylor9@dons.usfca.edu>
2026-02-04 15:34:16 -0800

    shell navbar

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,22 +1,21 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import CharacterCreation from './pages/character-creator'
-import Preview from './pages/preview'
-import Home from './pages/home'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import CharacterCreation from "./pages/character-creator";
+import Preview from "./pages/preview";
+import Home from "./pages/home";
 
 function App() {
-
   return (
     <div className="dark:bg-neutral-950 w-full h-full min-w-screen min-h-screen">
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home/>} />
-        <Route path="/dashboard" element={<>TODO</>} />
-        <Route path="/dashboard/create" element={<CharacterCreation/>} />
-        <Route path="/preview" element={<Preview/>} />
-      </Routes>
-    </BrowserRouter>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/dashboard" element={<>TODO</>} />
+          <Route path="/dashboard/create" element={<CharacterCreation />} />
+          <Route path="/preview" element={<Preview />} />
+        </Routes>
+      </BrowserRouter>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
